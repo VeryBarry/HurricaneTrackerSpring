c18171d8e5bba2a9d8c69d0b8a010a8f88147a49
oakes <zsoakes@gmail.com>
2016-10-21 12:08:38 -0400

    Add hibernate stuff

---
+++ src/main/java/com/theironyard/HurricaneRepository.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by zach on 10/21/16.
+ */
+public interface HurricaneRepository extends CrudRepository<Hurricane, Integer> {
+}
